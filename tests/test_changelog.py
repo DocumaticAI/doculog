d3f95a23a44003871380b89c330673ce7839a525
Tom Titcombe <t.j.titcombe@gmail.com>
2021-12-27 13:12:44 +0000

    Format code

--- tests/test_changelog.py
+++ tests/test_changelog.py
@@ -64,27 +64,55 @@ class TestChangelogSection:
 
 class TestChangelogRelease:
     def test_generate_gets_commits_up_to_release_date(self, mocker):
-        get_commits_mock = mocker.patch("autolog.changelog.get_commits", return_value=[])
+        get_commits_mock = mocker.patch(
+            "autolog.changelog.get_commits", return_value=[]
+        )
 
         release = ChangelogRelease("0.1.0", "2021-12-25")
         release.generate("2021-12-01")
-        get_commits_mock.assert_called_once_with(since_date="2021-12-01", until_date="2021-12-25")
+        get_commits_mock.assert_called_once_with(
+            since_date="2021-12-01", until_date="2021-12-25"
+        )
 
-    @pytest.mark.parametrize("keyword,change_type", (("Make", "Added"), ("Add", "Added"), ("Tweaked", "Changed"), ("Remove", "Removed"), ("Fix", "Fixed"), ("Deprecate", "Deprecated")))
+    @pytest.mark.parametrize(
+        "keyword,change_type",
+        (
+            ("Make", "Added"),
+            ("Add", "Added"),
+            ("Tweaked", "Changed"),
+            ("Remove", "Removed"),
+            ("Fix", "Fixed"),
+            ("Deprecate", "Deprecated"),
+        ),
+    )
     def test_catalog_commit_searches_first_word_for_keyword(self, keyword, change_type):
         commit = f"{keyword} a test commit"
         actual_type, _ = ChangelogRelease.catalog_commit(commit)
 
         assert actual_type == change_type
 
-    @pytest.mark.parametrize("keyword,change_type", (("make", "Added"), ("add", "Added"), ("tweaked", "Changed"), ("remove", "Removed"), ("fix", "Fixed"), ("deprecate", "Deprecated")))
-    def test_catalog_commit_searches_lowercase_first_word_for_keyword(self, keyword, change_type):
+    @pytest.mark.parametrize(
+        "keyword,change_type",
+        (
+            ("make", "Added"),
+            ("add", "Added"),
+            ("tweaked", "Changed"),
+            ("remove", "Removed"),
+            ("fix", "Fixed"),
+            ("deprecate", "Deprecated"),
+        ),
+    )
+    def test_catalog_commit_searches_lowercase_first_word_for_keyword(
+        self, keyword, change_type
+    ):
         commit = f"{keyword} a test commit"
         actual_type, _ = ChangelogRelease.catalog_commit(commit)
 
         assert actual_type == change_type
 
-    @pytest.mark.parametrize("firstword", ("Google", "Gazumped", "Tweet", "Did", "finally"))
+    @pytest.mark.parametrize(
+        "firstword", ("Google", "Gazumped", "Tweet", "Did", "finally")
+    )
     def test_catalog_commit_returns_None_if_first_word_not_keyword(self, firstword):
         commit = f"{firstword} a test commit"
         actual_type, actual_update = ChangelogRelease.catalog_commit(commit)
@@ -92,26 +120,52 @@ class TestChangelogRelease:
         assert actual_type is None
         assert actual_update is None
 
-    @pytest.mark.parametrize("update_type", ("Added", "Changed", "Fixed", "Deprecated", "Removed"))
+    @pytest.mark.parametrize(
+        "update_type", ("Added", "Changed", "Fixed", "Deprecated", "Removed")
+    )
     def test_read_populates_existing_section(self, update_type):
-        existing_updates = [f"### {update_type}\n", "", "* an update", "  * a sub update", "* another update message", "- a third update", "### Some other section", "* this update should not be included"]
+        existing_updates = [
+            f"### {update_type}\n",
+            "",
+            "* an update",
+            "  * a sub update",
+            "* another update message",
+            "- a third update",
+            "### Some other section",
+            "* this update should not be included",
+        ]
 
         release = ChangelogRelease("test", "2021-12-25")
         release.read(existing_updates)
 
         for _section_type, _section in release._sections.items():
             if _section_type == update_type:
-                assert _section._commits == ["* an update", "  * a sub update", "* another update message", "- a third update"]
+                assert _section._commits == [
+                    "* an update",
+                    "  * a sub update",
+                    "* another update message",
+                    "- a third update",
+                ]
             else:
                 assert _section._commits == []
 
     def test_read_populates_multiple_sections(self):
-        existing_updates = [f"### Added\n", "", "* an update", "  * a sub update", "### Removed", "* something"]
+        existing_updates = [
+            f"### Added\n",
+            "",
+            "* an update",
+            "  * a sub update",
+            "### Removed",
+            "* something",
+        ]
 
         release = ChangelogRelease("test", "2021-12-25")
         release.read(existing_updates)
 
-        assert release._sections["Added"]._commits == ["* an update", "  * a sub update"]
+        assert release._sections["Added"]._commits == [
+            "* an update",
+            "  * a sub update",
+        ]
         assert release._sections["Removed"]._commits == ["* something"]
 
     def test_reads_empty_section(self):
