0bcfec367ea55c4aa44c852bbab8ad4984eb46e9
Tom Titcombe <t.j.titcombe@gmail.com>
2021-12-27 12:43:36 +0000

    Add functionality to query server for data (POST)
    Will be used for advanced changelog features. Currently not integrated

--- autolog/requests.py
+++ autolog/requests.py
@@ -1,3 +1,75 @@
 """
 Call the server for intelligent changelog features.
 """
+import hashlib
+import json
+import os
+from typing import List, Optional, Union
+
+import requests
+
+SERVER_DOMAIN = "https://testurl/"
+
+
+def post(endpoint: str, payload: Union[str, List[str]]) -> Optional[Union[str, List[str]]]:
+    """
+    Call server with changelog info for advanced parsing features.
+
+    Parameters
+    ----------
+    endpoint : str
+        The endpoint to query
+    payload : str or list of str
+        The data to post to the endpoint
+
+    Returns
+    -------
+    None, str or list of str
+        None if POST fails or returns non-200 status code.
+        Otherwise, the parsed payload.
+
+    Notes
+    -----
+    * Sends a POST request to server
+    * Sends HASHED project name purely for usage logging purposes
+    * 
+    """
+    project_name = os.getenv("AUTOLOG_PROJECT_NAME")
+    project_name = project_name if project_name else "DefaultProject"
+
+    hashed_project = hashlib.sha224(
+        project_name.encode("utf-8")
+    ).hexdigest()
+
+    api_key = os.getenv("AUTOLOG_API_KEY")
+
+    _run_locally = (
+        os.getenv("AUTOLOG_RUN_LOCALLY") == "True"
+    ) or not api_key
+    _server_domain = SERVER_DOMAIN if not _run_locally else "http://127.0.0.1:3000/"
+
+    try:
+        response = requests.post(
+            _server_domain + endpoint + f"?project={hashed_project}",
+            data=json.dumps(payload),
+            headers={
+                "x-api-key": api_key,
+                "content-type": "application/json",
+            },
+        )
+    except requests.exceptions.ConnectionError:
+        # Happens if running locally but server hasn't set up (or API server has crashed!)
+        return None
+
+    if response.status_code == 200:
+        message = response.json()["message"]
+        if isinstance(message, list):
+            return message
+
+        try:
+            message = json.loads(message)
+        except Exception:
+            pass
+        return message
+    else:
+        return None
---
+++ tests/test_requests.py
@@ -0,0 +1,247 @@
+import hashlib
+import json
+import os
+from unittest import mock
+
+import pytest
+import responses
+
+from autolog.requests import SERVER_DOMAIN, post
+
+
+class TestPost:
+    @responses.activate
+    @pytest.mark.parametrize("endpoint", ("", "tester", "summarise_function"))
+    def test_posts_to_endpoint(self, endpoint):
+        responses.add(
+            responses.POST, SERVER_DOMAIN + endpoint, json={"message": "test"}
+        )
+        os.environ["AUTOLOG_API_KEY"] = "12345"
+        os.environ["AUTOLOG_PROJECT_NAME"] = "Autolog"
+
+        post(endpoint, "test payload")
+
+        assert len(responses.calls) == 1
+        assert responses.calls[0].request.url.split("/")[-1].split("?")[0] == endpoint
+
+    @responses.activate
+    @mock.patch.dict(
+        os.environ,
+        {
+            "AUTOLOG_API_KEY": "12345",
+            "AUTOLOG_PROJECT_NAME": "Autolog",
+            "AUTOLOG_RUN_LOCALLY": "False",
+        },
+    )
+    def test_receives_and_parses_list(self):
+        responses.add(
+            responses.POST,
+            SERVER_DOMAIN + "test",
+            json={"message": json.dumps([1, 2, 3, 4])},
+        )
+
+        response = post("test", "test payload")
+        assert response == [1, 2, 3, 4]
+
+    @responses.activate
+    @mock.patch.dict(
+        os.environ,
+        {
+            "AUTOLOG_API_KEY": "12345",
+            "AUTOLOG_PROJECT_NAME": "Autolog",
+            "AUTOLOG_RUN_LOCALLY": "False",
+        },
+    )
+    def test_receives_and_parses_string(self):
+        responses.add(
+            responses.POST,
+            SERVER_DOMAIN + "test",
+            json={"message": json.dumps("some string result")},
+        )
+
+        response = post("test", "test payload")
+        assert response == "some string result"
+
+    @responses.activate
+    @mock.patch.dict(
+        os.environ,
+        {
+            "AUTOLOG_API_KEY": "12345",
+            "AUTOLOG_PROJECT_NAME": "Autolog",
+            "AUTOLOG_RUN_LOCALLY": "False",
+        },
+    )
+    def test_receives_and_parses_dict(self):
+        responses.add(
+            responses.POST,
+            SERVER_DOMAIN + "test",
+            json={
+                "message": json.dumps({"summary": "some string result", "num_calls": 1})
+            },
+        )
+
+        response = post("test", "test payload")
+        assert response == {"summary": "some string result", "num_calls": 1}
+
+    @responses.activate
+    @pytest.mark.parametrize(
+        "project_name", ("some-project", "MyProj!", "A.Cool.Project")
+    )
+    def test_posts_with_hashed_project_name(self, project_name):
+        endpoint = "test"
+        responses.add(
+            responses.POST, SERVER_DOMAIN + endpoint, json={"message": "test"}
+        )
+        os.environ["AUTOLOG_API_KEY"] = "12345"
+        os.environ["AUTOLOG_PROJECT_NAME"] = project_name
+
+        expected_hash = hashlib.sha224(project_name.encode("utf-8")).hexdigest()
+
+        post(endpoint, "test payload")
+        actual_request = responses.calls[0].request
+
+        assert actual_request.params["project"] == expected_hash
+        assert len(actual_request.params) == 1
+
+    @responses.activate
+    def test_posted_with_hashed_project_default_if_env_var_not_set(self):
+        if "AUTOLOG_PROJECT_NAME" in os.environ:
+            del os.environ["AUTOLOG_PROJECT_NAME"]
+
+        endpoint = "test"
+
+        responses.add(
+            responses.POST, SERVER_DOMAIN + endpoint, json={"message": "test"}
+        )
+
+        expected_hash = hashlib.sha224("DefaultProject".encode("utf-8")).hexdigest()
+
+        post(endpoint, "test payload")
+        actual_request = responses.calls[0].request
+
+        assert actual_request.params["project"] == expected_hash
+        assert len(actual_request.params) == 1
+
+    @responses.activate
+    def test_can_post_list_data(self):
+        endpoint = "test"
+        responses.add(
+            responses.POST, SERVER_DOMAIN + endpoint, json={"message": "test"}
+        )
+        os.environ["AUTOLOG_API_KEY"] = "12345"
+        os.environ["AUTOLOG_PROJECT_NAME"] = "some-project"
+
+        post(endpoint, ["some", "data"])
+        assert len(responses.calls) == 1
+
+    @responses.activate
+    def test_can_post_str_data(self):
+        endpoint = "test"
+        responses.add(
+            responses.POST, SERVER_DOMAIN + endpoint, json={"message": "test"}
+        )
+        os.environ["AUTOLOG_API_KEY"] = "12345"
+        os.environ["AUTOLOG_PROJECT_NAME"] = "some-project"
+
+        post(endpoint, "import json\n\nclass A:\npass")
+        assert len(responses.calls) == 1
+
+    @responses.activate
+    @pytest.mark.parametrize("api_key", ("12345", "aggj!33"))
+    def test_reads_api_from_environ_and_includes_in_header(self, api_key):
+        endpoint = "test"
+        responses.add(
+            responses.POST, SERVER_DOMAIN + endpoint, json={"message": "test"}
+        )
+        os.environ["AUTOLOG_API_KEY"] = api_key
+        os.environ["AUTOLOG_PROJECT_NAME"] = "MyProject"
+
+        post(endpoint, "test payload")
+        actual_request = responses.calls[0].request
+
+        assert actual_request.headers["x-api-key"] == api_key
+
+    @responses.activate
+    @mock.patch.dict(
+        os.environ,
+        {
+            "AUTOLOG_API_KEY": "12345",
+            "AUTOLOG_PROJECT_NAME": "some-proj",
+            "AUTOLOG_RUN_LOCALLY": "True",
+        },
+    )
+    def test_calls_local_host_if_env_var_set_to_true(self):
+        endpoint = "test"
+        responses.add(
+            responses.POST,
+            f"http://127.0.0.1:3000/{endpoint}",
+            json={"message": "test"},
+        )
+
+        post(endpoint, "test payload")
+        assert len(responses.calls) == 1
+
+    @responses.activate
+    @mock.patch.dict(
+        os.environ,
+        {
+            "AUTOLOG_PROJECT_NAME": "some-proj",
+            "AUTOLOG_RUN_LOCALLY": "False",
+        },
+    )
+    def test_calls_local_if_run_local_is_false_but_api_key_not_set(self):
+        if "AUTOLOG_API_KEY" in os.environ:
+            del os.environ["AUTOLOG_API_KEY"]
+
+        endpoint = "test"
+        responses.add(
+            responses.POST,
+            f"http://127.0.0.1:3000/{endpoint}",
+            json={"message": "test"},
+        )
+
+        post(endpoint, "test payload")
+        assert len(responses.calls) == 1
+
+    @responses.activate
+    @mock.patch.dict(
+        os.environ,
+        {
+            "AUTOLOG_API_KEY": "12345",
+            "AUTOLOG_PROJECT_NAME": "some-proj",
+            "AUTOLOG_RUN_LOCALLY": "False",
+        },
+    )
+    def test_calls_server_if_env_var_set_to_false(self):
+        endpoint = "test"
+        responses.add(
+            responses.POST, SERVER_DOMAIN + endpoint, json={"message": "test"}
+        )
+
+        post(endpoint, "test payload")
+        assert len(responses.calls) == 1
+
+    @responses.activate
+    @mock.patch.dict(
+        os.environ, {"AUTOLOG_API_KEY": "12345", "AUTOLOG_PROJECT_NAME": "some-proj"}
+    )
+    def test_calls_server_if_env_var_not_set(self):
+        endpoint = "test"
+        responses.add(
+            responses.POST, SERVER_DOMAIN + endpoint, json={"message": "test"}
+        )
+
+        post(endpoint, "test payload")
+        assert len(responses.calls) == 1
+
+    @mock.patch.dict(
+        os.environ,
+        {
+            "AUTOLOG_API_KEY": "12345",
+            "AUTOLOG_PROJECT_NAME": "some-proj",
+            "AUTOLOG_RUN_LOCALLY": "True",
+        },
+    )
+    def test_returns_None_if_cannot_connect_to_server(self):
+        endpoint = "test"
+        assert post(endpoint, "test payload") is None
