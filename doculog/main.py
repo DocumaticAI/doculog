11093638ca5f34cd9e800dacac2f491a52715ae9
Seniatical <isaja7710@gmail.com>
2022-01-24 20:32:16 +0000

    Implement a proper CLI

--- doculog/main.py
+++ doculog/main.py
@@ -2,9 +2,10 @@
 CLI entrypoints to the code.
 """
 from pathlib import Path
+from argparse import ArgumentParser
 
-from doculog.changelog import ChangelogDoc
 from doculog.config import configure
+from doculog import ChangelogDoc, __version__
 
 
 def generate_changelog():
@@ -19,3 +20,28 @@ def generate_changelog():
 
     if log_path.exists():
         print(f"Saved changelog to {log_path}")
+
+
+parser = ArgumentParser(
+    prog=f"Doculog v{__version__}",
+)
+
+parser.add_argument("--cl", "-change-log", 
+    action="store_true",
+    dest="cl",
+    help="Generates changelog for project"
+)
+
+def parse():
+    args = {k: v for k, v in vars(parser.parse_args()).items() if v is not None}
+
+    if not args:
+        # Some message or default text displayed here maybe
+        # Command Used: doculog
+        exit(0)
+
+    if args["cl"]:
+        # Called when --cl or -change-log flag is used
+        generate_changelog()
+
+    exit(0)
